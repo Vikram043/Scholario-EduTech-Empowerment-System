3f430ceb6f7229b777b604acd363b23a0e0832e4
Vikram Naik <knowme962@gmail.com>
2023-09-01 22:57:40 +0530

    admin model html added

--- Server/student_management_system/student_management_app/urls.py
+++ Server/student_management_system/student_management_app/urls.py
@@ -8,11 +8,37 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home_view,name=''),  
 
-
+    path('adminclick', views.adminclick_view),
     path('teacherclick', views.teacherclick_view),
     path('studentclick', views.studentclick_view),     
 
-
+    path('adminsignup', views.admin_signup_view),
     path('studentsignup', views.student_signup_view,name='studentsignup'),
     path('teachersignup', views.teacher_signup_view),
+    path('adminlogin', LoginView.as_view(template_name='school/adminlogin.html')),
+    path('studentlogin', LoginView.as_view(template_name='school/studentlogin.html')),
+    path('teacherlogin', LoginView.as_view(template_name='school/teacherlogin.html')),
+
+
+    
+    path('admin-teacher', views.admin_teacher_view,name='admin-teacher'),
+    path('admin-add-teacher', views.admin_add_teacher_view,name='admin-add-teacher'),
+    path('admin-view-teacher', views.admin_view_teacher_view,name='admin-view-teacher'),
+    path('admin-approve-teacher', views.admin_approve_teacher_view,name='admin-approve-teacher'),
+    path('approve-teacher/<int:pk>', views.approve_teacher_view,name='approve-teacher'),
+    path('delete-teacher/<int:pk>', views.delete_teacher_view,name='delete-teacher'),
+    path('delete-teacher-from-school/<int:pk>', views.delete_teacher_from_school_view,name='delete-teacher-from-school'),
+    path('update-teacher/<int:pk>', views.update_teacher_view,name='update-teacher'),
+    path('admin-view-teacher-salary', views.admin_view_teacher_salary_view,name='admin-view-teacher-salary'),
+
+
+
+    path('teacher-dashboard', views.teacher_dashboard_view,name='teacher-dashboard'),
+    path('teacher-attendance', views.teacher_attendance_view,name='teacher-attendance'),
+    path('teacher-take-attendance/<str:cl>', views.teacher_take_attendance_view,name='teacher-take-attendance'),
+    path('teacher-view-attendance/<str:cl>', views.teacher_view_attendance_view,name='teacher-view-attendance'),
+    path('teacher-notice', views.teacher_notice_view,name='teacher-notice'),
+
+    path('student-dashboard', views.student_dashboard_view,name='student-dashboard'),
+    path('student-attendance', views.student_attendance_view,name='student-attendance'),
 ]
